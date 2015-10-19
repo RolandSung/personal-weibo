f2144a2043b337cdc1936ef1899c8658ddb054a8
RolandSung <mrsongxr@163.com>
2015-10-19 13:37:01 +0800

    将第三方框架，新建分类加入版本控制

---
+++ 私人定制-微博/classes/Others/Category/UIImage+RSImage.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+RSImage.h
+//  私人定制-微博
+//
+//  Created by imac on 15/10/14.
+//  Copyright © 2015年 Roland Sung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (RSImage)
+
++ (instancetype)imageWithStretchableImageName:(NSString *)imageName;
+
+
+@end
